6d4fccb67cdc550d7284588e72bfa99239611b8a
Madhubala1993 <madhu.a1993@gmail.com>
2022-03-01 20:48:25 +0530

    helper

--- helper.js
+++ helper.js
@@ -1,13 +1,10 @@
 import { client } from "./index.js";
 
 export async function addRepo(newrepo) {
-  return await client.db("mongo_db").collection("aws").insertMany(newrepo);
+  return await client.db("aws").collection("aws").insertMany(newrepo);
 }
 
 export async function getAllRepos(request) {
-  return await client
-    .db("mongo_db")
-    .collection("aws")
-    .find(request.query)
-    .toArray();
+  return await client.db("aws").collection("aws").find(request.query).toArray();
 }
+  
